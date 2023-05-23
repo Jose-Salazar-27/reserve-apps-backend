dbb3c7d265b05b3c9bb4b746befa4b7d90549a59
Jose Andres Salazar <joseandres61@gmail.com>
2023-05-23 16:28:51 -0400

    fix some data modifier in PrismaClient class

--- src/prisma/client.ts
+++ src/prisma/client.ts
@@ -5,7 +5,7 @@ import { DBConnector } from "../shared/adapters/db-connection-adapter";
 // Singletoon pattern, to avoid multiple or unexpected connections
 export class PrismaClient extends Configuration implements DBConnector {
   private client: Prisma;
-  private instance?: PrismaClient;
+  private static instance?: PrismaClient;
   private constructor(client: Prisma) {
     super();
     this.client = client;
@@ -19,7 +19,7 @@ export class PrismaClient extends Configuration implements DBConnector {
     await this.client.$disconnect();
   }
 
-  public useInstance(): PrismaClient {
+  static useInstance(): PrismaClient {
     if (!this.instance) {
       this.instance = new PrismaClient(new Prisma());
       return this.instance;
