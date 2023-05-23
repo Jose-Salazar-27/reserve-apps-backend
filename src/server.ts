eef75721dc992d359f5d6cea2dc41477efa0a19e
Jose Andres Salazar <joseandres61@gmail.com>
2023-05-23 16:18:09 -0400

    chore: add start app functionality

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,24 @@
-import express, { Application } from 'express';
-import cors from 'cors';
-import { Configuration } from './config/server-config';
+import express, { Application } from "express";
+import cors from "cors";
+import { Server as HttpType } from "http";
+import { Configuration } from "./config/server-config";
 
 export class Server extends Configuration {
   private app: Application;
+  private server?: HttpType;
   constructor() {
     super();
     this.app = express();
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(cors());
+  }
+
+  public async start(): Promise<void> {
+    if (!this.server) {
+      this.server = this.app.listen(super.getPort(), () => {
+        console.log(`Server is running on port ${super.getPort()} `);
+      });
+    }
   }
 }
