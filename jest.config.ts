3e12517aad427673ed2f78ec599cc1ae2fdb9d52
Jose Andres Salazar <joseandres61@gmail.com>
2023-06-22 19:48:40 -0400

    chore: modify jest.config

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,12 @@
-import type { Config } from "jest";
+import type { Config } from 'jest';
 
-export default async (): Promise<Config> => {
-  return {
-    verbose: true,
-    detectLeaks: true,
-  };
+const config: Config = {
+  verbose: true,
+  // detectLeaks: true,
+  clearMocks: true,
+  testEnvironment: 'node',
+  preset: 'ts-jest',
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
 };
+
+export default config;
