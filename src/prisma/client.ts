1cc91d256da6c66c0b7378796222bc70447b5820
Jose Andres Salazar <joseandres61@gmail.com>
2023-06-22 20:01:35 -0400

    chore: create singleton prisma instance

--- src/prisma/client.ts
+++ src/prisma/client.ts
@@ -1,30 +1,4 @@
-import { PrismaClient as Prisma } from "@prisma/client";
-import { Configuration } from "../config/server-config";
-import { DBConnector } from "../shared/adapters/db-connection-adapter";
+import { PrismaClient } from "@prisma/client";
 
 // Singletoon pattern, to avoid multiple or unexpected connections
-export class PrismaClient extends Configuration implements DBConnector {
-  private client: Prisma;
-  private static instance?: PrismaClient;
-  private constructor(client: Prisma) {
-    super();
-    this.client = client;
-  }
-
-  public async connect(): Promise<void> {
-    await this.client.$connect();
-  }
-
-  public async disconnect(): Promise<void> {
-    await this.client.$disconnect();
-  }
-
-  static useInstance(): PrismaClient {
-    if (!this.instance) {
-      this.instance = new PrismaClient(new Prisma());
-      return this.instance;
-    }
-
-    return this.instance;
-  }
-}
+export const prisma = new PrismaClient();
